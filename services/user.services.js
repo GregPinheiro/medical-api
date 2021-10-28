c937d3da4de4d06d6a134de1baaf999e924025a0
Greg <gpinheirojunior@gmail.com>
2021-10-28 13:28:35 -0300

    Implementacao: rota POST do User

--- services/user.services.js
+++ services/user.services.js
@@ -4,6 +4,38 @@ var jwt = require("jsonwebtoken");
 const { secret } = require("../middlewares/config");
 
 const userServices = {
+  post: async (datas) => {
+    let status = null;
+    let error = null;
+    let data = {};
+
+    const { name, password } = datas;
+
+    const hashPassword = bcrypt.hashSync(password, 10);
+
+    try {
+      const checkName = await User.findOne({ where: { name } });
+
+      if (checkName) {
+        status = 400;
+        error = "Usuário já cadastrado!";
+      } else {
+        const response = await User.create({ name, password: hashPassword });
+
+        const token = jwt.sign({ id: response.id }, secret, {
+          expiresIn: 3600,
+        });
+
+        status = 201;
+        data = { auth: true, token, name };
+      }
+    } catch (e) {
+      status = 500;
+      error = e;
+    }
+
+    return { data, status, error };
+  },
   signin: async (datas, session) => {
     let status = null;
     let error = null;
